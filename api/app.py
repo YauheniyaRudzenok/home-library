c229cd714d7e662c62d054aef125cf5997afc27f
MasDenBy <horssupport@gmail.com>
2020-05-08 17:48:32 +0200

    api: fix configuration

--- api/app.py
+++ api/app.py
@@ -15,4 +15,4 @@ db.init_app(app)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=50000)
--- api/services/config_service.py
+++ api/services/config_service.py
@@ -4,8 +4,7 @@ import os
 class ConfigService():
 
     def ds_ip(self):
-        return 'masden-server'
-        #return self.__env_or_default__('HL_DS_IP', '127.0.0.1')
+        return self.__env_or_default__('HL_DS_IP', '127.0.0.1')
 
 
     def ds_port(self):
@@ -13,18 +12,15 @@ class ConfigService():
 
 
     def ds_user(self):
-        return 'homelibrary'
-        #return self.__env_or_default__('HL_DS_USER', 'admin')
+        return self.__env_or_default__('HL_DS_USER', 'admin')
 
 
     def ds_password(self):
-        return 'yc8DNa8MU2ZUwc^'
-        #return os.environ('HL_DS_USER_PASSWORD')
+        return os.environ('HL_DS_USER_PASSWORD')
 
 
     def library_path(self):
-        return '/Library'
-        #return os.environ('HL_PATH')
+        return os.environ('HL_PATH')
 
 
     def db_path(self):
@@ -33,10 +29,6 @@ class ConfigService():
 
     def base_path(self):
         return os.getcwd()
-
-
-    # def books_search_pattern(self):
-    #     return 
 
 
     def __env_or_default__(self, key, default):
